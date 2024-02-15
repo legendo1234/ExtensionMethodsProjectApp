7e1902343ea3c4530670acbb48125665da068057
Matej Weber <matej.weber@gmail.com>
2024-02-15 19:45:41 +0100

    Add project files.

---
+++ ExtensionMethodsProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            
+            person.FirstName = "What is your first name : ".RequestString();
+
+           
+            person.LastName = "What is your Last name : ".RequestString();
+
+
+            person.Age = "What is your Age : ".RequestInt(0,120);
+
+
+            Console.ReadLine();
+        }
+    }
+}
